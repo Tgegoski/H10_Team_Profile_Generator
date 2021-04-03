c390bb9988e69225e1ffe6bf6d00e360a6bf946a
Tammy Gegoski <tgegoski@gmail.com>
2021-04-02 20:40:40 -0400

    set up prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,73 @@
 const inquirer = require ("inquirer");
 const jest = require ("jest");
-const employee = require ("./Employee");
-
+const fs = require ("fs");
+const employee = require ("./lib/Employee");
+const employee = require ("./lib/Engineer");
+const employee = require ("./lib/Intern");
+const employee = require ("./lib/Manager");
 const employee = newEmployee();
+
+inquirer
+    .promptEmployee([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your employees name?',
+        },
+        {
+            type: 'input',
+            id: 'employeeID?',
+            message: 'What is your employees id?',
+        },
+        {
+            type: 'input',
+            email: 'employees email',
+            message: 'What is your employees email?',
+        
+        },
+        {
+            type: 'input',
+            role: 'employeeRole',
+            message: 'What is your employees role?',
+            choices: ['Engineer, Intern, Manager'],
+        
+        },
+    ])
+
+        inquirer 
+        .promptEngineer([
+        {
+            type: 'input',
+            gitHub: 'gitHubUserName',
+            message: 'What is your Engineers Git Hub User Name?',
+        
+        },
+    ])
+
+        inquirer 
+        .promptIntern([
+
+        {
+            type: 'input',
+            school: 'schoolName',
+            message: 'What is the name of your Interns School?',
+        
+        },
+    ])
+
+        inquirer 
+        .promptManager([
+        {
+            type: 'input',
+            office: 'officeNumber',
+            message: 'What is your Managers office number?',
+        },
+             
+    ]          
+    
+).then ((
+    answers
+) =>{
+
+} )
 
